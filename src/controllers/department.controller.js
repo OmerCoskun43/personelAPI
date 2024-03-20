6bbe4cad532ae96863fe7fdedf5ae59c0c7d1bd4
AdamSmith <omercoskun4369@gmail.com>
2024-03-20 16:25:10 +0300

    department structure updated

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -38,31 +38,43 @@ module.exports = {
   update: async (req, res) => {
     const id = req.params?.id;
     const newData = req.body;
-    await Department.updateOne({ _id: id }, newData);
-    const data = await Department.findOne({ _id: id });
+    await Department.updateOne({ _id: id }, newData, { runValidators: true });
 
-    res.status(200).send({
+    res.status(202).send({
       error: false,
       message: "Department updated succesfully",
       details: await res.getModelListDetails(Department),
-      data,
+      data: await Department.findOne({ _id: id }),
     });
   },
   delete: async (req, res) => {
     const id = req.params?.id;
     const data = await Department.findOne({ _id: id });
-    if (!data) {
-      res.errorStatusCode = 404;
-      throw new Error("There is not such department with this ID");
-    } else {
-      await Department.deleteOne({ _id: id });
+    //! Birinci Yöntem
+    // if (!data) {
+    //   res.errorStatusCode = 404;
+    //   throw new Error("There is not such department with this ID");
+    // } else {
+    //   await Department.deleteOne({ _id: id });
 
-      res.status(200).send({
-        error: false,
-        message: "Department updated succesfully",
-        details: await res.getModelListDetails(Department),
-        data,
-      });
-    }
+    //   res.status(204).send({
+    //     error: false,
+    //     message: "Department updated succesfully",
+    //     details: await res.getModelListDetails(Department),
+    //     data,
+    //   });
+    // }
+
+    //! İkinci Yöntem
+
+    const dataa = await Department.deleteOne({ _id: id });
+
+    res.status(dataa.deletedCount > 0 ? 204 : 404).send({
+      error: !dataa.deletedCount,
+      message: dataa.deletedCount
+        ? "Department deleted succesfully"
+        : "There is not such an Department with this ID",
+      data,
+    });
   },
 };
